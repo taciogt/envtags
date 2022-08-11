3fa77c05c925c6fd8ed472da38613f5b9527063e
Tácio Tavares <taciogt@gmail.com>
2022-08-10 22:05:38 -0300

    fixes tess

--- env_test.go
+++ env_test.go
@@ -36,7 +36,7 @@ func TestSet(t *testing.T) {
 		//Set(s)  TODO: what happens here?
 		Set(&s)
 
-		if s.Foo != "bar b" {
+		if s.Foo != "bar" {
 			t.Errorf("unexpected env var set. expected=\"bar\". got=\"%s\"", s.Foo)
 		}
 	})
