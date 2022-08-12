db108bd040d745164ec61e08590d5f3d94283308
Tácio Tavares <taciogt@gmail.com>
2022-08-11 22:26:37 -0300

    clean commented code

--- env.go
+++ env.go
@@ -1,7 +1,6 @@
 package envtags
 
 import (
-	"fmt"
 	"os"
 	"reflect"
 	"strconv"
@@ -11,33 +10,28 @@ const tagName = "env"
 
 func Set(s interface{}) error {
 	value := reflect.ValueOf(s)
-	//if value.Type().Kind() == reflect.Ptr {
-	//
-	//	elem := value.Elem()
-	//}
 	elem := value.Elem()
-
-	//typeSpec := value.Type()
 	typeSpec := elem.Type()
+
 	for i := 0; i < elem.NumField(); i++ {
 		f := elem.Field(i)
-		fmt.Println("struct field", f)
 		fType := typeSpec.Field(i)
-		fmt.Printf("struct type tag: %+v\n", fType.Tag)
-		fmt.Printf("struct type tag.env: %+v\n", fType.Tag.Get(tagName))
 
 		envVarName := fType.Tag.Get(tagName)
-		envVarValue, _ := os.LookupEnv(envVarName)
+		envVarValue, ok := os.LookupEnv(envVarName)
 
-		if fType.Type.Kind() == reflect.String {
-			f.SetString(envVarValue)
-		} else if fType.Type.Kind() == reflect.Int {
-			invValue, err := strconv.Atoi(envVarValue)
-			if err != nil {
-				return err
+		if ok {
+			if fType.Type.Kind() == reflect.String {
+				f.SetString(envVarValue)
+			} else if fType.Type.Kind() == reflect.Int {
+				invValue, err := strconv.Atoi(envVarValue)
+				if err != nil {
+					return err
+				}
+				f.SetInt(int64(invValue))
 			}
-			f.SetInt(int64(invValue))
 		}
+
 	}
 	return nil
 }
--- env_test.go
+++ env_test.go
@@ -5,80 +5,50 @@ import (
 	"testing"
 )
 
-func TestSet(t *testing.T) {
-	//tests := []struct {
-	//	name string
-	//	input interface{}
-	//	want bool
-	//}{
-	//	// TODO: Add test cases.
-	//}
-	//for _, tt := range tests {
-	//	t.Run(tt.name, func(t *testing.T) {
-	//		if got := Set(); got != tt.want {
-	//			t.Errorf("Set() = %v, want %v", got, tt.want)
-	//		}
-	//	})
-	//}
+func TestSetFieldTypes(t *testing.T) {
+	type Config struct {
+		Word string `env:"FOO"`
 
-	t.Run("set string field from envvar", func(t *testing.T) {
-		if err := os.Setenv("FOO", "bar"); err != nil {
-			t.Error(err)
-			return
-		}
+		Number int `env:"NUMBER"`
+	}
 
-		type S struct {
-			Foo string `env:"FOO"`
-		}
+	tests := []struct {
+		name     string
+		expected Config
+		envVars  map[string]string
+		wantErr  error
+	}{{
+		name:     "set string field",
+		expected: Config{Word: "bar"},
+		envVars: map[string]string{
+			"FOO": "bar",
+		},
+	}, {
+		name:     "set integer field",
+		expected: Config{Number: 123},
+		envVars: map[string]string{
+			"NUMBER": "123",
+		},
+	},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for k, v := range tt.envVars {
+				if err := os.Setenv(k, v); err != nil {
+					t.Error(err)
+					return
+				}
+			}
+			defer os.Clearenv()
 
-		var s S
+			var cfg Config
 
-		//Set(s)  TODO: what happens here?
-		Set(&s)
-
-		if s.Foo != "bar" {
-			t.Errorf("unexpected env var set. expected=\"bar\". got=\"%s\"", s.Foo)
-		}
-	})
-
-	t.Run("set int field from envvar", func(t *testing.T) {
-		if err := os.Setenv("SOME_NUMBER", "123"); err != nil {
-			t.Error(err)
-			return
-		}
-
-		type S struct {
-			Number int `env:"SOME_NUMBER"`
-		}
-
-		var s S
-
-		//Set(s)  TODO: what happens here?
-		Set(&s)
-
-		if s.Number != 123 {
-			t.Errorf("unexpected env var set. expected=\"123\". got=\"%d\"", s.Number)
-		}
-	})
-
-	//t.Run("set int field from invalid integer envvar", func(t *testing.T) {
-	//	if err := os.Setenv("SOME_NUMBER", "abc"); err != nil {
-	//		t.Error(err)
-	//		return
-	//	}
-	//
-	//	type S struct {
-	//		Number int `env:"SOME_NUMBER"`
-	//	}
-	//
-	//	var s S
-	//
-	//	//Set(s)  TODO: what happens here?
-	//	Set(&s)
-	//
-	//	if s.Number != 123 {
-	//		t.Errorf("unexpected env var set. expected=\"123\". got=\"%d\"", s.Number)
-	//	}
-	//})
-
+			if err := Set(&cfg); err != tt.wantErr {
+				t.Errorf("err different than expected, want %+v, got %+v", tt.wantErr, err)
+			}
+			if cfg != tt.expected {
+				t.Errorf("Set(&s), want %+v, got %+v", tt.expected, cfg)
+			}
+		})
+	}
 }
